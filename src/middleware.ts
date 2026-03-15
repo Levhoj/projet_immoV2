faa514802d8057c4da93268c05e084717bdc65e2
Levhoj <titouan.duboss@gmail.com>
2026-03-15 14:38:19 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,5 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-  '/outils(.*)',
-])
-
-const isPublicRoute = createRouteMatcher([
-  '/',
-  '/login(.*)',
-  '/tarifs(.*)',
-])
-
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req) && isProtectedRoute(req)) {
-    await auth.protect()
-  }
-})
+export default function middleware() {}
 
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
+  matcher: [],
 }
